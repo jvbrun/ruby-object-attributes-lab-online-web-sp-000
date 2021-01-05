4442438090f5bf14fc753aa95da71dd946c91378
Jean-Victor Brun <jeanvbrun@icloud.com>
2021-01-05 05:27:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,18 @@
 class Dog 
  
 #  def initialize(name)
-#    @name = name
-#  end
- 
-#  def name
-#    @name
-#  end
- 
+
   def name=(name)
     @name = name
   end
+  
+#    @name = name
+#  end
+ 
+  def name
+    @name
+  end
+ 
+
   
 end
